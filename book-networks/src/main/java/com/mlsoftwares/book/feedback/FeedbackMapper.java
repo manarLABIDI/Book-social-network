206c633d89fb0e5e82e67678122228e8c8fc09ed
manarLABIDI <manarabidi4@gmail.com>
2024-10-19 15:45:27 +0100

    Implmenet feedback feature

---
+++ book-networks/src/main/java/com/mlsoftwares/book/feedback/FeedbackMapper.java
@@ -0,0 +1,27 @@
+package com.mlsoftwares.book.feedback;
+
+import com.mlsoftwares.book.book.Book;
+
+import java.util.Objects;
+
+public class FeedbackMapper {
+    public Feedback tofeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .archived(false)
+                        .shareable(false)
+                        .build())
+                .build();
+    }
+
+    public FeedbackResponse tofeedbackResponse(Feedback f, Integer id) {
+       return FeedbackResponse.builder()
+               .note(f.getNote())
+               .comment(f.getComment())
+               .ownFeedback(Objects.equals(f.getCreatedBy(), id))
+                .build();
+    }
+}
---
+++ book-networks/src/main/java/com/mlsoftwares/book/feedback/FeedbackResponse.java
@@ -0,0 +1,18 @@
+package com.mlsoftwares.book.feedback;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FeedbackResponse {
+
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
+}
