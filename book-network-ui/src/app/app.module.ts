5ef87be3258059d8eedd050f4d89b6799ce2b863
manarLABIDI <manarabidi4@gmail.com>
2024-10-21 00:47:13 +0100

    Implement interceptors in ui

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app.routes';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import { CodeInputModule } from 'angular-code-input';
+import { HttpTokenInterceptor } from './services/services/interceptor/http-token.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,11 @@ import { CodeInputModule } from 'angular-code-input';
     ],
   providers: [
     HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi:true // because it's not the only interceptor in the app
+    }
 
   ],
   bootstrap: [AppComponent]
