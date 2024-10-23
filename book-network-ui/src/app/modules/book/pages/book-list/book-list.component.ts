91703cfe78b7f95ff500c00b03d0d9dc1f6ebf6b
manarLABIDI <manarabidi4@gmail.com>
2024-10-23 20:11:55 +0100

    Implement pagination in UI

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -12,7 +12,7 @@ export class BookListComponent implements OnInit{
 
   bookResponse: PageResponseBookResponse = {};
   page = 0;
-  size = 2;
+  size = 1;
   pages: any = [];
   message = '';
   level: 'success' |'error' = 'success';
