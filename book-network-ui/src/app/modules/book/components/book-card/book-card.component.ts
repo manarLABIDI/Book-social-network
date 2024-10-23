06aa12cac814d21726a9e113965f3942e1b4414e
manarLABIDI <manarabidi4@gmail.com>
2024-10-23 19:33:36 +0100

    Implement pagination in UI

--- book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
+++ book-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
@@ -22,8 +22,8 @@ export class BookCardComponent {
 
  
   get bookCover(): string | undefined {
-    if (this._book.cover) {
-        return 'data:image/jpg;base64,' + this._book.cover;
+    if (this._bookCover) {
+        return 'data:image/jpg;base64,' + this._bookCover;
     }
     return 'https://picsum.photos/1900/800';
 }
--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../../../services/services';
 import { Router } from '@angular/router';
-import { PageResponseBookResponse } from '../../../../services/models';
+import { BookResponse, PageResponseBookResponse } from '../../../../services/models';
 
 @Component({
   selector: 'app-book-list',
@@ -9,32 +9,91 @@ import { PageResponseBookResponse } from '../../../../services/models';
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit{
-  bookResponse: PageResponseBookResponse = {};
 
-  page: number = 0;
-  size: number = 5;
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 2;
+  pages: any = [];
+  message = '';
+  level: 'success' |'error' = 'success';
+
   constructor(
     private bookService: BookService,
     private router: Router
   ) {
-    
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.findAllBooks();
-
   }
 
   private findAllBooks() {
-    this.bookService.findAllBooks(
-     { page: this.page,
+    this.bookService.findAllBooks({
+      page: this.page,
       size: this.size
-    }).subscribe( {
-      next:(books) => {
-        this.bookResponse = books;
-
-      }
     })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
+  }
 
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.level = 'success';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book successfully added to your list';
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+    });
+  }
+
+  displayBookDetails(book: BookResponse) {
+    this.router.navigate(['books', 'details', book.id]);
   }
 }
+
+
+
+
